65197354dba0f80263641c948266bf87cb34aa1e
Yannick <girofare@hotmail.com>
2024-02-13 23:54:33 +0100

    added Image

--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 export default function First_post() {
   return ( <>
+  <img src="/images/index.jpg" alt="Your Name" />
   <h1>First post </h1>
   <Link href={'/'}>
     Return to home !
