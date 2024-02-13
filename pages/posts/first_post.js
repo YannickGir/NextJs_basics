0be5138488cd69b1c9b31ec25a82c3b7cffb0c7a
Yannick <girofare@hotmail.com>
2024-02-14 00:01:20 +0100

    added Head

--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -1,16 +1,16 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Head from "next/head";
 
 export default function First_post() {
-  return ( <>
-  <img src="/images/index.jpg" alt="Your Name" />
-  <h1>First post </h1>
-  <Link href={'/'}>
-    Return to home !
-  </Link>
-  </>
-    
-
-
-  ) 
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <img src="/images/index.jpg" alt="Your Name" />
+      <h1>First post </h1>
+      <Link href={"/"}>Return to home !</Link>
+    </>
+  );
 }
