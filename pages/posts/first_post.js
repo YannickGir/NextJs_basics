90d38d926b9383c28a01ddbd6eb27f02de53bcc2
Yannick <girofare@hotmail.com>
2024-02-16 19:15:57 +0100

    added layout

--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -1,16 +1,23 @@
 import Link from "next/link";
 import React from "react";
 import Head from "next/head";
+import Layout from '../../components/layout';
+
 
 export default function First_post() {
   return (
     <>
-      <Head>
+    <Layout>
+          <Head>
         <title>First Post</title>
+        
       </Head>
+     
       <img src="/images/index.jpg" alt="Your Name" />
       <h1>First post </h1>
       <Link href={"/"}>Return to home !</Link>
+    </Layout>
+    
     </>
   );
 }
