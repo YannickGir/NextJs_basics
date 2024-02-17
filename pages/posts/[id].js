fdfd4b018bc456cdf785184f54a15df2b5fd0014
Yannick <girofare@hotmail.com>
2024-02-17 19:06:58 +0100

    added css in dynamic file

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -22,26 +23,19 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post({postData}) {
+export default function Post({ postData }) {
   return (
-    <>
     <Layout>
-          <Head>
+      <Head>
         <title>{postData.title}</title>
-        
       </Head>
-     
-      <img src="/images/index.jpg" alt="Your Name" />
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date}/>
-      
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
-    
-    </>
   );
 }
